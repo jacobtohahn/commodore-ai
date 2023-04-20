08fc96a42eb07a31d2439456b5e0005d76d6a46b
Jacob Hahn <jtohahn@gmail.com>
2023-04-20 14:17:13 -0400

    Additional constraints

--- constraints_capabilities.py
+++ constraints_capabilities.py
@@ -5,11 +5,20 @@ capabilities_generator = CapabilitiesGenerator()
 capabilities_generator.add_constraint("""
 Only use your available capabilites
 """)
+capabilities_generator.add_constraint("""
+Respond with a clear, singular action to perform
+""")
+capabilities_generator.add_constraint("""
+Write files in .md Markdown format only
+""")
+capabilities_generator.add_constraint("""
+Remember your previous actions and don't repeat steps
+""")
 
 capabilities_generator.add_capability("""
 Filesystem operations, including reading, writing, appending, and deleting files,
 creating, moving, and deleting directories
 """)
 capabilities_generator.add_capability("""
-Internet access by googleing a search term or browsing a website URL
+Internet access by googling a search term or browsing a website URL
 """)
