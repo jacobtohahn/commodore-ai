f23090272122769d0b29a279ca6f628ba669f06a
Jacob Hahn <jtohahn@gmail.com>
2023-04-21 14:03:38 -0400

    Shortened webpage chunk length

--- processing/text.py
+++ processing/text.py
@@ -56,7 +56,7 @@ def summarize_text(
     print(f"Text length: {text_length} characters")
 
     summaries = []
-    chunks = list(split_text(text))
+    chunks = list(split_text(text, 4000))
     scroll_ratio = 1 / len(chunks)
 
     for i, chunk in enumerate(chunks):
